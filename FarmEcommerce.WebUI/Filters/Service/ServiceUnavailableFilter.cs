c27b8f9f6537c6f6784bfd891deaaae82e62295c
EricCatayas <catayasericjay@gmail.com>
2024-04-16 10:15:20 +0800

    Implemented ServiceUnavailable resource filter

---
+++ FarmEcommerce.WebUI/Filters/Service/ServiceUnavailableFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FarmEcommerce.WebUI.Filters.Service
+{
+    /// <summary>
+    /// A resource filter that short-circuits the request and returns a status code 503 Service Unavailable response
+    /// </summary>
+    public class ServiceUnavailableFilter : IResourceFilter
+    {
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.Result = new StatusCodeResult(503);            
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+
+        }
+    }
+}

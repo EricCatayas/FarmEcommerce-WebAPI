0025e6dd082214e8800c01b4140bc18e8c8be204
EricCatayas <catayasericjay@gmail.com>
2024-04-16 10:18:33 +0800

    Added DataNotFound exception documentation in IProductGetService

--- FarmEcommerce.Core/ServiceContracts/Products/IProductGetService.cs
+++ FarmEcommerce.Core/ServiceContracts/Products/IProductGetService.cs
@@ -1,7 +1,11 @@
 ﻿using FarmEcommerce.Core.Common.DTO;
+using FarmEcommerce.Core.Common.Exceptions;
 
 namespace FarmEcommerce.Core.ServiceContracts.Products
 {
+    /// <exception>
+    /// Throws <see cref="DataNotFoundException"/> if product does with corresponding Id does not exist in the database.
+    /// </exception>
     public interface IProductGetService
     {
         public Task<ProductDTO> GetProduct(int product_id);        

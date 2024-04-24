c4caa125c418f8cca13585681cea75308d6d31e2
EricCatayas <catayasericjay@gmail.com>
2024-04-24 14:57:16 +0800

    Converted ImageUploadCreateDTO from class to record

--- FarmEcommerce.Core/Common/DTO/ImageUploadCreateDTO.cs
+++ FarmEcommerce.Core/Common/DTO/ImageUploadCreateDTO.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace FarmEcommerce.Core.Common.DTO
 {
-    public class ImageUploadCreateDTO
+    public record ImageUploadCreateDTO
     {
         public int Images_Id { get; set; }
         public string Image_Url { get; set; }

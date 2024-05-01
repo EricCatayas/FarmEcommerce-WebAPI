32fd9d7b0342d673d89dbe3c4ca53ddf43ccf65f
EricCatayas <catayasericjay@gmail.com>
2024-05-01 15:39:40 +0800

    Refactored code in CreateProductWithImagesCommand

--- FarmEcommerce.WebUI/Commands/Products/CreateProductWithImagesCommand.cs
+++ FarmEcommerce.WebUI/Commands/Products/CreateProductWithImagesCommand.cs
@@ -15,11 +15,12 @@ namespace FarmEcommerce.WebUI.Commands.Products
     public class CreateProductAndUploadImagesCommand : IRequest<ProductDTO>
     {
         private ProductCreateDTO product;
-        public CreateProductAndUploadImagesCommand(ProductCreateDTO product)
+        public IEnumerable<IFormFile>? image_Files { get; set; }
+        public CreateProductAndUploadImagesCommand(ProductCreateDTO product, IEnumerable<IFormFile> Image_Files)
         {
             this.product = product;
+            image_Files = Image_Files;
         }
-        public IEnumerable<IFormFile>? image_Files { get; set; }
         public ProductCreateDTO GetProductCreateDTO()
         {
             return this.product;
@@ -27,13 +28,13 @@ namespace FarmEcommerce.WebUI.Commands.Products
     }
     public class CreateProductAndUploadImagesCommandHandler : IRequestHandler<CreateProductAndUploadImagesCommand, ProductDTO>
     {
-        private readonly IProductCreateService _createService;
+        private readonly IProductCreateService _productCreateService;
         private readonly IImageUploadCreateService _imageUploadCreateService;
         private readonly IImageUploadService _imageUploadService;
 
         public CreateProductAndUploadImagesCommandHandler(IProductCreateService productCreateService, IImageUploadCreateService imageUploadCreateService, IImageUploadService imageUploadService)
         {
-            _createService = productCreateService;
+            _productCreateService = productCreateService;
             _imageUploadCreateService = imageUploadCreateService;
             _imageUploadService = imageUploadService;
         }
@@ -42,15 +43,16 @@ namespace FarmEcommerce.WebUI.Commands.Products
         {
             try
             {
-                if (request.image_Files == null)
+                if (request.image_Files.IsNullOrEmpty())
                     throw new ArgumentException("Product images must not be null.");
 
-                var product = await _createService.AddAsync(request.GetProductCreateDTO());
-                // Upload image
-                if (!request.image_Files.IsNullOrEmpty())
-                {
-                    product.Images = await UploadImagesAsync(product.GetImagesID(), request.image_Files);
-                }
+                else if(!ValidImageTypes(request.image_Files))
+                    throw new ArgumentException("Product images must be of valid image type.");
+
+                var product = await _productCreateService.AddAsync(request.GetProductCreateDTO());
+                
+                // Upload images
+                product.Images = await UploadImagesAsync(product.GetImagesID(), request.image_Files);
 
                 return product;
             }
@@ -59,24 +61,31 @@ namespace FarmEcommerce.WebUI.Commands.Products
                 throw;
             }
         }
+
+        private bool ValidImageTypes(IEnumerable<IFormFile> image_Files)
+        {
+            foreach (var imageFile in image_Files)
+                if (!ImageFileValidator.IsValidImageFile(imageFile))
+                    return false;
+
+            return true;
+        }
+
         private async Task<IEnumerable<ImageUploadDTO>> UploadImagesAsync(int imagesId, IEnumerable<IFormFile> imageFiles)
         {
-            var imageFilesInBytes = new List<ImageUploadCreateDTO>();
+            var imageUploadList = new List<ImageUploadCreateDTO>();
             foreach (var imageFile in imageFiles)
-            {
-                if (ImageFileValidator.IsValidImageFile(imageFile))
+            {                
+                var result = await _imageUploadService.UploadAsync(imageFile);
+                imageUploadList.Add(new ImageUploadCreateDTO()
                 {
-                    var result = await _imageUploadService.UploadAsync(imageFile);
-                    imageFilesInBytes.Add(new ImageUploadCreateDTO()
-                    {
-                        Images_Id = imagesId,
-                        Image_Url = result,                        
-                    });
-                }
+                    Images_Id = imagesId,
+                    Image_Url = result,                        
+                });
 
             }
 
-            return await _imageUploadCreateService.AddRangeAsync(imageFilesInBytes);
+            return await _imageUploadCreateService.AddRangeAsync(imageUploadList);
         }
     }
 }
